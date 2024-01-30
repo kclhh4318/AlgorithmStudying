f14fca973a504c8888056d07620a77a2d46fe000
lehyno_ <kclhh4318@hanyang.ac.kr>
2024-01-30 15:55:51 +0900

    2023.01.30

--- loopInvariant.cpp
+++ loopInvariant.cpp
@@ -17,3 +17,17 @@ int binsearch(const vector<int>& A, int x){
     }
     return hi;
 }
+
+void insertionSort(vector<int>& A){
+    for(int i = 0; i < A.size(); ++i){
+        //불변식 a: A[0..i - 1]은 이미 정렬되어 있다.
+        //A[0..i - 1]에 A[i]를 끼워넣는다.
+        int j = i;
+        while(j > 0 && A[j - 1] > A[j]){
+            //불변식 b: A[j + 1..i]의 모든 원소는 A[j]보다 크다.
+            //불변식 c: A[0..i] 구간은 A[j]를 제외하면 정렬되어 있다.
+            swap(A[j - 1], A[j]);
+            --j;
+        }
+    }
+}
