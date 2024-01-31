e2ecda8eb95a1db9d4f92668f1d716a93122f620
lehyno_ <kclhh4318@hanyang.ac.kr>
2024-01-31 18:02:03 +0900

    2023.01.31

--- recursive.cpp
+++ recursive.cpp
@@ -51,3 +51,51 @@ bool hasWord(int y, int x, const string& word){
     }
     return false;
 }
+
+//잘못된 재귀 호출의 예
+int n;
+bool areFriends[10][10];
+//taken[i] = i번째 학생이 짝을 이미 찾았으면 true, 아니면 false
+int countPairings(bool taken[10]){
+    //기저 사례: 모든 학생이 짝을 찾았으면 한 가지 방법을 찾았으니 종료한다.
+    bool finished = true;
+    for(int i = 0; i < n; ++i) if(!taken[i]) finished = false;
+    if(finished) return 1;
+    int ret = 0;
+    // 서로 친구인 두 학생을 찾아 짝을 지어 준다.
+    for(int i = 0; i < n; ++i)
+        for(int j = 0; j < n; ++j)
+            if(!taken[i] && !taken[j] && areFriends[i][j]){
+                taken[i] = taken[j] = true;
+                ret += countPairings(taken);
+                taken[i] = taken[j] = false;
+            }
+    return ret;
+}
+
+//소풍 문제를 해결하는 재귀 호출 코드
+
+int n;
+bool areFriends[10][10];
+//taken[i] = i번째 학생이 짝을 이미 찾았으면 true, 아니면 false
+int countPairings(bool taken[10]){
+    //남은 학생 중 가장 번호가 빠른 학생을 찾는다.
+    int firstFree = -1;
+    for(int i = 0; i < n; ++i){
+        if(!taken[i]){
+            firstFree = i;
+            break;
+        }
+    }
+    //기저 사례: 모든 학생이 짝을 찾았으면 한 가지 방법을 찾았으니 종료한다.
+    if(firstFree == -1) return 1;
+    int ret = 0;
+    //이 학생과 짝지을 학생을 결정한다.
+    for(int pairWith = firstFree + 1; pairWith < n; ++pairWith){
+        if(!taken[pairWith] && areFriends[firstFree][pairWith]){
+            taken[firstFree] = taken[pairWith] = true;
+            ret += countPairings(taken);
+            taken[firstFree] = taken[pairWith] = false;
+        }
+    }
+}
