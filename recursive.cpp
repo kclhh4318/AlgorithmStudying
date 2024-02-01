e1e58246ff0efa850b3f26814610b6bcf4ba4492
lehyno_ <kclhh4318@hanyang.ac.kr>
2024-02-01 23:37:19 +0900

    2024.02.01

--- recursive.cpp
+++ recursive.cpp
@@ -121,9 +121,9 @@ bool set(vector<vector<int>>& board, int y, int x, int type, int delta){
     for(int i = 0; i < 3; ++i){
         const int ny = y + coverType[type][i][0];
         const int nx = x + coverType[type][i][1];
-        if(ny < 0 || ny >= board.size() || nx < 0 || nx >= board[0].size())
+        if(ny < 0 || ny >= board.size() || nx < 0 || nx >= board[0].size()) //게임판 범위를 넘어섰을때
             ok = false;
-        else if((board[ny][nx] += delta) > 1)
+        else if((board[ny][nx] += delta) > 1) // 겹칠때
             ok = false;
     }
     return ok;
@@ -153,6 +153,32 @@ int cover(vector<vector<int>>& board){
             ret += cover(board);
         //덮었던 블록을 치운다.
         set(board, y, x, type, -1);
+    }
+    return ret;
+}
+
+int n; // 도시의 수
+double dist[MAX][MAX]; // 두 도시간의 거리를 저장하는 배열
+//path: 지금까지 만든 경로
+//visited: 각 도시의 방문 여부
+//currentLength: 지금까지 만든 경로의 길이
+//나머지 도시들을 모두 방문하는 경로들 중 가장 짧은 것의 길이를 반환한다.
+int shortestPath(vector<int>& path, vector<bool>& visited, double currentLength){
+    //기저 사례: 모든 도시를 다 방문했을 때는 시작 도시로 돌아가고 종료한다.
+    if(path.size() == n)
+        return currentLength + dist[path[0]][path.back()];
+    double ret = INF; // 매우 큰 값으로 초기화
+    //다음 방문할 도시를 전부 시도해 본다.
+    for(int next = 0; next < n; ++next){
+        if(visited[next]) continue;
+        int here = path.back();
+        path.push_back(next);
+        visited[next] = true;
+        //나머지 경로를 재귀 호출을 통해 완성하고 가장 짧은 경로의 길이를 얻는다.
+        double cand = shortestPath(path, visited, currentLength + dist[here][next]);
+        ret = min(ret, cand);
+        visited[next] = false;
+        path.pop_back();
     }
     return ret;
 }
