b5ba067a9174615d4842c0712b1e640f6fc57688
lehyno_ <kclhh4318@hanyang.ac.kr>
2024-02-04 23:08:41 +0900

    2024.02.04

--- divide&conquer.cpp
+++ divide&conquer.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <string>
 #include <cmath>
+using namespace std;
 
 //필수 조건: n은 자연수
 //1 + 2 + ... + n을 반환한다.
@@ -143,3 +144,127 @@ int bruteForce(const vector<int>& h){
     return ret;
 }
 
+int hugs(const string& members, const string& fans){
+    int N = members.size(), M = fans.size();
+    vector<int> A(N), B(M);
+    for(int i = 0; i < N; ++i) A[i] = (members[i] == 'M');
+    for(int i = 0; i < M; ++i) B[M - i - 1] = (fans[i] == 'M');
+    //karatsuba 알고리즘에서 자릿수 올림은 생략한다.
+    vector<int> C = karatsuba(A, B);
+    int allHugs = 0;
+    for(int i = N - 1; i < M; ++i)
+        if(C[i] == 0)
+            ++allHugs;
+    return allHugs;
+}
+
+// -1로 초기화 해준다.
+int cache[30][30];
+int bino2(int n, int r){
+    //기저 사례
+    if(r == 0 || n == r) return 1;
+    // -1이 아니라면 한 번 계산했던 값이니 곧장 반환
+    if(cache[n][r] != -1) return cache[n][r];
+    //직접 계산한 뒤 배열에 저장
+    return cache[n][r] = bino2(n - 1, r - 1) + bino2(n - 1, r);
+}
+
+//전부 -1로 초기화 해준다.
+int cache[2500][2500];
+//a와 b는 각각 [0, 2500) 구간 안의 정수
+//반환 값은 항상 int 형 안에 들어가는 음이 아닌 정수
+int someObscureFunction(int a, int b){
+    //기저 사례를 처음에 처리한다.
+    if(...) return ...;
+    //(a, b)에 대한 답을 구한 적이 있으면 곧장 반환
+    int& ret = cache[a][b];
+    if(ret != -1) return ret;
+    //여기에서 답을 계산한다.
+    ...
+    return ret;
+
+}
+
+int main(){
+    //memst()를 이용해 cache를 -1로 초기화한다.
+    memset(cache, -1, sizeof(cache));
+}
+
+int n, board[100][100];
+bool jump(int y, int x){
+    //기저 사례: 게임판 밖을 벗어난 경우
+    if(y >= n || x >= n) return false;
+    //기저 사례; 마지막 칸에 도착한 경우
+    if(y == n - 1 && x == n - 1) return true;
+    int jumpSize = board[y][x];
+    return jump(y + jumpSize, x) || jump(y, x + jumpSize);
+}
+
+int cache[100][100];
+int jump2(int y, int x){
+    //기저 사례 처리
+    if(y >= n || x >= n) return 0;
+    if(y == n - 1 && x == n - 1) return 1;
+    //메모이제이션
+    int& ret = cache[y][x];
+    if(ret != -1) return ret;
+    int jumpSize = board[y][x];
+    return ret = (jump(y + jumpSize, x) || jump2(y, x + jumpSize));
+}
+
+//와일드카드 패턴w가 문자열s에 대응되는지 여부를 반환한다.
+bool match(const string& w, const string& s){
+    //w[pos]와 s[pos]를 맞춰나간다.
+    int pos = 0;
+    while(pos < s.size() && pos < w.size() && (w[pos] == '?' || w[pos] == s[pos]))
+    ++pos;
+    ...
+}
+
+//와일드카드 패턴 w가 문자열 s에 대응되는지 여부를 반환한다.
+bool match(const string& w, const string& s){
+    //w[pos]와 s[pos]를 맞춰나간다.
+    int pos = 0;
+    while(pos < s.size() && pos < w.size() && (w[pos] == '?' || w[pos] == s[pos])) ++pos;
+    //더이상 대응할 수 없으면 왜 while문이 끝났는지 확인한다.
+    //2. 패턴 끝에 도달해서 끝난 경우: 문자열도 끝났어야 대응됨
+    if(pos == w.size()) return pos == s.size();
+    //4. *를 만나서 끝난 경우: *에 몇 글자를 대응해야 할지 재귀 호출하면서 확인한다.
+    if(w[pos] == '*')
+        for(int skip = 0; pos + skip <= s.size(); ++skip)
+            if(match(w.substr(pos + 1), s.substr(pos + skip)))
+                return true;
+    //이 외의 경우에는 모두 대응되지 않는다.
+    return false;
+}
+
+//-1은 아직 답이 계산되지 않았음을 의미한다.
+//1은 해당 입력들이 서로 대응됨을 의미한다.
+//0은 해당 입력들이 서로 대응되지 않음을 의미한다.
+int cache[101][101];
+//패턴과 문자열
+string W, S;
+//와일드카드 패턴 W[w..]가 문자열 S[s..]에 대응되는지 여부를 반환한다.
+bool matchMemoized(int w, int s){
+    //메모이제이션
+    int& ret = cache[w][s];
+    if(ret != -1) return ret;
+    //W[w]와 S[s]를 맞춰나간다.
+    while(s < S.size() && w < W.size() && (W[w] == '?' || W[w] == S[s])){
+        ++w;
+        ++s;
+    }
+    //더이상 대응할 수 없으면 왜 while문이 끝났는지 확인한다.
+    //2. 패턴 끝에 도달해서 끝난 경우: 문자열도 끝났어야 대응됨
+    if(w == W.size()) return ret = (s == S.size());
+    //4. *를 만나서 끝난 경우: *에 몇 글자를 대응해야 할지 재귀 호출하면서 확인한다.
+    if(W[w] == '*')
+        for(int skip = 0; skip + s <= S.size(); ++skip)
+            if(matchMemoized(w + 1, s + skip))
+                return ret = 1;
+    //3. 이 외의 경우에는 모두 대응되지 않는다.
+    return ret = 0;
+}
+
+if(w < W.size() && s < S.size() && (W[w] == '?' || W[w] == S[s]))
+    return ret = matchMemoized(w + 1, s + 1);
